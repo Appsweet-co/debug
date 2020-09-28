2efe03e0aa420a529aea0c5163f28d16b47bed44
dperuo <derek@derekperuo.net>
2020-09-28 12:31:11 -0500

    docs: ✏️ () Hide internals and dependencies

--- lib/debug/service.ts
+++ lib/debug/service.ts
@@ -8,26 +8,33 @@ export interface DebugConfig {
   meta?: boolean;
 }
 
+/** @internal */
 export const defaults: DebugConfig = {
   disabled: [],
   meta: false,
   prefix: null,
 };
 
+/** @internal */
 export const maybeDisabled = (val: DebugType[]) => maybe(defaults.disabled, val);
 
+/** @internal */
 export const maybeMeta = (active: boolean) => maybe(defaults.meta, active);
 
+/** @internal */
 export const maybePrefix = (prefix: string) => maybe(defaults.prefix, prefix);
 
+/** @internal */
 export const deactivate = (list: DebugType[]) => {
   return (kind: DebugType) => list.includes('all') || list.includes(kind);
 };
 
+/** @internal */
 export const format = (prefix: string|null|undefined) => {
   return (...context: any) => (prefix) ? [`[${prefix}]`, ...context] : [...context];
 };
 
+/** @internal */
 export const meta = (active: boolean|undefined) => {
   return (kind: DebugType) => (active) ? [ `==> [${kind.toUpperCase()}]` ] : [];
 };

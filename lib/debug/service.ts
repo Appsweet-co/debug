fbc92d4d048d1ec414696f7a16712f6f4f850517
dperuo <derek@derekperuo.net>
2021-07-14 21:02:36 -0500

    refactor: (service.ts) Use true-myth

--- lib/debug/service.ts
+++ lib/debug/service.ts
@@ -1,28 +1,14 @@
-import { maybe } from '@appsweet-co/utils';
-
-export type DebugType = 'all' | 'log' | 'warn' | 'error';
-
-export interface DebugConfig {
-  prefix?: string | null;
-  disabled?: DebugType[];
-  meta?: boolean;
-}
+import { Maybe } from 'true-myth/maybe';
+import { DebugType, defaults } from './const';
 
 /** @internal */
-export const defaults: DebugConfig = {
-  disabled: [],
-  meta: false,
-  prefix: null,
-};
+export const maybeDisabled = (val: DebugType[]) => Maybe.of(val).unwrapOr(defaults.disabled);
 
 /** @internal */
-export const maybeDisabled = (val: DebugType[]) => maybe(defaults.disabled, val);
+export const maybeMeta = (active: boolean) => Maybe.of(active).unwrapOr(defaults.meta);
 
 /** @internal */
-export const maybeMeta = (active: boolean) => maybe(defaults.meta, active);
-
-/** @internal */
-export const maybePrefix = (prefix: string) => maybe(defaults.prefix, prefix);
+export const maybePrefix = (prefix: string) => Maybe.of(prefix).unwrapOr(defaults.prefix);
 
 /** @internal */
 export const deactivate = (list: DebugType[]) => {
@@ -30,11 +16,11 @@ export const deactivate = (list: DebugType[]) => {
 };
 
 /** @internal */
-export const format = (prefix: string|null|undefined) => {
+export const format = (prefix: string | null | undefined) => {
   return (...context: any) => (prefix) ? [`[${prefix}]`, ...context] : [...context];
 };
 
 /** @internal */
-export const meta = (active: boolean|undefined) => {
-  return (kind: DebugType) => (active) ? [ `==> [${kind.toUpperCase()}]` ] : [];
+export const meta = (active: boolean | undefined) => {
+  return (kind: DebugType) => (active) ? [`==> [${kind.toUpperCase()}]`] : [];
 };
